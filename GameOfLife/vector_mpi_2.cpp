48919fc84d9526c2476e8ba42f79fc90e963b8f3
CherryMellonTree <stijn.jacobs.37@outlook.com>
2024-12-06 22:07:03 +0100

    halfway there to finally fixing mpi

--- GameOfLife/vector_mpi_2.cpp
+++ GameOfLife/vector_mpi_2.cpp
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <iomanip>
 #include <vector>
+#include <mpi.h>
 
 #define COLOR_RED "\033[31m"
 #define COLOR_GREEN "\033[32m"
@@ -16,106 +17,157 @@
 //view influence of more cores/threads on VSC
 //Why do we even use MPI?
 
-void printGrid(const std::vector<bool>& grid, int xsize, int ysize);
-void determineState(std::vector<bool>& grid, int xsize, int ysize);
+void printGrid(const std::vector<char>& grid, int xsize, int ysize);
+void determineState(std::vector<char>& grid, int xsize, int ysize);
 void clearScreen(void);
-void printGridToFile(const std::vector<bool>& grid, int xsize, int ysize, const std::string& filename);
-std::vector<bool> initializeGrid(int size);
+void printGridToFile(const std::vector<char>& grid, int xsize, int ysize, const std::string& filename);
+std::vector<char> initializeGrid(int size);
 inline int getIndex(int x, int y, int xsize, int ysize);
 
 int main(int argc, char* argv[]) {
-    clearScreen();
+    int global_comm_sz;
+    int comm_sz;
+    int my_rank;
 
-    std::cout << COLOR_BLUE;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    auto grid = initializeGrid(GRID_SIZE);
-    int x, y;
-
-    std::string number_of_cells;
-    std::string start;
-    std::string filename;
-
-    if (argc == 2) {
-        filename = argv[1];
-        std::ifstream readfile(filename);
-        if (readfile.is_open()) {
-            std::string fileline, xx, yy;
-            while (std::getline(readfile, fileline)) {
-                std::stringstream ss(fileline);
-                std::getline(ss, xx, ' ');
-                std::getline(ss, yy, ' ');
-
-                x = std::stoi(xx);
-                y = std::stoi(yy);
-
-                grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = true;
-            }
-        }
-        start = "y";
-    } else {
-        std::cout << "Enter the number of cells, or 'r' to read cells from file: ";
-        std::cin >> number_of_cells;
-        std::cout << std::endl;
-
-        if (number_of_cells == "r") {
-            while (true) {
-                std::cout << "Enter name of file to read from: ";
-                std::cin >> filename;
-
-                std::ifstream readfile(filename);
-                if (readfile.is_open()) {
-                    std::string fileline, xx, yy;
-                    while (std::getline(readfile, fileline)) {
-                        std::stringstream ss(fileline);
-                        std::getline(ss, xx, ' ');
-                        std::getline(ss, yy, ' ');
-
-                        x = std::stoi(xx);
-                        y = std::stoi(yy);
-
-                        grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = true;
-                    }
-                    break;
-                } else {
-                    std::cout << "File not found :(" << std::endl;
-                }
-            }
-        } else {
-            for (int i = 0; i < std::stoi(number_of_cells); i++) {
-                std::cout << "Cell " << i + 1 << "/" << number_of_cells << "\nEnter the x & y coordinates: ";
-                std::cin >> x >> y;
-                grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = true;
-                printGrid(grid, GRID_SIZE, GRID_SIZE);
-            }
-        }
-        std::cout << "Grid setup is done. Start the game? (y/n): ";
-        std::cin >> start;
+    if( GRID_SIZE %(comm_sz-1) != 0){
+        return 1;
     }
 
-    int incrementor = 0;
-    if (start == "y" || start == "Y") {
-        printGridToFile(grid, GRID_SIZE, GRID_SIZE, "./outputs/output_grid_000000");
+    int rowcount_per_mpi = GRID_SIZE /(comm_sz-1);
+    int y_size = GRID_SIZE;
+    int x_size = rowcount_per_mpi;
+    int elements_received_count = (rowcount_per_mpi+2) * y_size; //+2 for 1 buffer on each side
 
-        while (true) {
-            printGrid(grid, GRID_SIZE, GRID_SIZE);
-            determineState(grid, GRID_SIZE, GRID_SIZE);
+    if(my_rank==0){
+        clearScreen();
 
-            std::ostringstream filenameStream;
-            filenameStream << "./outputs/output_grid_" << std::setw(4) << std::setfill('0') << incrementor << ".txt";
-            std::string outputFilename = filenameStream.str();
-            printGridToFile(grid, GRID_SIZE, GRID_SIZE, outputFilename);
-            incrementor++;
+        std::cout << COLOR_BLUE;
 
-            usleep(200000);
-            clearScreen();
+        auto grid = initializeGrid(GRID_SIZE);
+        int x, y;
+
+        std::string number_of_cells;
+        std::string start;
+        std::string filename;
+
+        if (argc == 2) {
+            filename = argv[1];
+            std::ifstream readfile(filename);
+            if (readfile.is_open()) {
+                std::string fileline, xx, yy;
+                while (std::getline(readfile, fileline)) {
+                    std::stringstream ss(fileline);
+                    std::getline(ss, xx, ' ');
+                    std::getline(ss, yy, ' ');
+
+                    x = std::stoi(xx);
+                    y = std::stoi(yy);
+
+                    grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = '1';
+                }
+            }
+            start = "y";
+        } else {
+            std::cout << "Enter the number of cells, or 'r' to read cells from file: ";
+            std::cin >> number_of_cells;
+            std::cout << std::endl;
+
+            if (number_of_cells == "r") {
+                while (true) {
+                    std::cout << "Enter name of file to read from: ";
+                    std::cin >> filename;
+
+                    std::ifstream readfile(filename);
+                    if (readfile.is_open()) {
+                        std::string fileline, xx, yy;
+                        while (std::getline(readfile, fileline)) {
+                            std::stringstream ss(fileline);
+                            std::getline(ss, xx, ' ');
+                            std::getline(ss, yy, ' ');
+
+                            x = std::stoi(xx);
+                            y = std::stoi(yy);
+
+                            grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = '1';
+                        }
+                        break;
+                    } else {
+                        std::cout << "File not found :(" << std::endl;
+                    }
+                }
+            } else {
+                for (int i = 0; i < std::stoi(number_of_cells); i++) {
+                    std::cout << "Cell " << i + 1 << "/" << number_of_cells << "\nEnter the x & y coordinates: ";
+                    std::cin >> x >> y;
+                    grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = '1';
+                    printGrid(grid, GRID_SIZE, GRID_SIZE);
+                }
+            }
+            std::cout << "Grid setup is done. Start the game? (y/n): ";
+            std::cin >> start;
         }
-    } else {
-        return 0;
+
+        int incrementor = 0;
+        if (start == "y" || start == "Y") {
+            printGridToFile(grid, GRID_SIZE, GRID_SIZE, "./outputs/output_grid_000000");
+
+            while (true) {
+                printGrid(grid, GRID_SIZE, GRID_SIZE);
+                for (int rank = 1; rank < comm_sz; ++rank) {
+                    int start = getIndex((rank - 1) * rowcount_per_mpi -1, 0, x_size, y_size);
+                    int end = getIndex((rank) * rowcount_per_mpi, GRID_SIZE, x_size, y_size) ;
+                    int diff = end - start;
+                    std::cout << "Process Rank: " << my_rank << "\n"
+                    << "Variable Location: " << &start << " Name: start Value: " << start << "\n"
+                    << "Variable Location: " << &end << " Name: end Value: " << end << "\n"
+                    << "Variable Location: " << &(diff) << " Name: end - start Value: " << (end - start) << "\n"
+                    << "Variable Location: " << &elements_received_count << " Name: elements_received_count Value: " << elements_received_count << "\n"
+                    << "---------------------------------------------" << std::endl;
+
+                    MPI_Send(grid.data() + start, end - start, MPI_CHAR, rank, 0, MPI_COMM_WORLD);
+                }
+
+                for (int rank = 1; rank < comm_sz; ++rank) {
+                    int start = getIndex((rank - 1) * (elements_received_count), 0, x_size, y_size);
+                    int end = getIndex((rank) * elements_received_count, GRID_SIZE, x_size, y_size) ;
+
+                    MPI_Recv(grid.data() + start, end - start, MPI_CHAR, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                }
+                std::ostringstream filenameStream;
+                filenameStream << "./outputs/output_grid_" << std::setw(4) << std::setfill('0') << incrementor << ".txt";
+                std::string outputFilename = filenameStream.str();
+                printGridToFile(grid, GRID_SIZE, GRID_SIZE, outputFilename);
+                incrementor++;
+
+                usleep(200000);
+                clearScreen();
+            }
+        } else {
+            return 0;
+        }
+    }else{
+        while (true){
+            usleep(200000 * my_rank);
+
+            std::vector<char> localGrid(elements_received_count);
+
+            MPI_Recv(localGrid.data(), elements_received_count, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            // determineState(localGrid, x_size, y_size);
+
+            MPI_Send(localGrid.data() + y_size, x_size * y_size, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+
+        }
     }
 }
 
-std::vector<bool> initializeGrid(int size) {
-    return std::vector<bool>((size) * (size), false);
+std::vector<char> initializeGrid(int size) {
+    return std::vector<char>((size) * (size), '0');
 }
 
 inline int getIndex(int x, int y, int xsize, int ysize) {
@@ -129,16 +181,16 @@ void clearScreen(void) {
     std::cout << "\033[2J;" << "\033[1;1H"; // clears screen and moves cursor to home
 }
 
-void printGrid(const std::vector<bool>& grid, int xsize, int ysize) {
+void printGrid(const std::vector<char>& grid, int xsize, int ysize) {
     for (int a = 0; a < xsize; a++) {
         for (int b = 0; b < ysize; b++) {
-            std::cout << (grid[getIndex(a, b, xsize, ysize)] ? " O " : " . ");
+            std::cout << (grid[getIndex(a, b, xsize, ysize)]);
         }
         std::cout << std::endl;
     }
 }
 
-void printGridToFile(const std::vector<bool>& grid, int xsize, int ysize, const std::string& filename) {
+void printGridToFile(const std::vector<char>& grid, int xsize, int ysize, const std::string& filename) {
     std::ofstream outFile(filename);
     if (!outFile.is_open()) {
         std::cerr << "Error: Could not open file " << filename << " for writing." << std::endl;
@@ -147,31 +199,31 @@ void printGridToFile(const std::vector<bool>& grid, int xsize, int ysize, const 
 
     for (int a = 0; a < xsize; a++) {
         for (int b = 0; b < ysize; b++) {
-            outFile << (grid[getIndex(a, b, xsize, ysize)] ? " O " : " . ");
+            outFile << grid[getIndex(a, b, xsize, ysize)];
         }
         outFile << std::endl;
     }
     outFile.close();
 }
 
-void determineState(std::vector<bool>& grid, int xsize, int ysize) {
+void determineState(std::vector<char>& grid, int xsize, int ysize) {
     auto copy = grid;
 
-    for (int a = 0; a < xsize; a++) {
-        for (int b = 0; b < ysize; b++) {
+    for (int a = 1; a < xsize; a++) {
+        for (int b = 1; b < ysize; b++) {
             int alive = 0;
             for (int c = -1; c <= 1; c++) {
                 for (int d = -1; d <= 1; d++) {
-                    if (!(c == 0 && d == 0)) {
+                    if (!(c == '0' && d == '0')) {
                         alive += copy[getIndex(a + c, b + d, xsize, ysize)];
                     }
                 }
             }
 
             if (alive < 2 || alive > 3) {
-                grid[getIndex(a, b, xsize, ysize)] = false;
+                grid[getIndex(a, b, xsize, ysize)] = '0';
             } else if (alive == 3) {
-                grid[getIndex(a, b, xsize, ysize)] = true;
+                grid[getIndex(a, b, xsize, ysize)] = '1';
             }
         }
     }
