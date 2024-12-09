680e04c065379df016db8c0e4bd3b65d1ab82bd4
CherryMellonTree <stijn.jacobs.37@outlook.com>
2024-12-09 13:36:33 +0100

    beeg file version

--- GameOfLife/vector_mpi_2.cpp
+++ GameOfLife/vector_mpi_2.cpp
@@ -7,13 +7,15 @@
 #include <iomanip>
 #include <vector>
 #include <mpi.h>
+#include "mpi_timer.cpp"
 
 #define COLOR_RED "\033[31m"
 #define COLOR_GREEN "\033[32m"
 #define COLOR_BLUE "\033[34m"
 #define COLOR_RESET "\033[0m"
+#define ITERATION_COUNT 32
 #define MAX_ITERATIONS 32
-#define GRID_SIZE 32*32
+#define GRID_SIZE 32*32*32
 //view influence of more cores/threads on VSC
 //Why do we even use MPI?
 
@@ -46,7 +48,8 @@ int main(int argc, char* argv[]) {
     int elements_received_count = (rowcount_per_mpi+2) * y_size; //+2 for 1 buffer on each side
 
     if(my_rank==0){
-        clearScreen();
+        // clearScreen();
+        AutoAverageTimer mainTimer("main_program_timer");
 
         std::cout << COLOR_BLUE;
 
@@ -56,166 +59,172 @@ int main(int argc, char* argv[]) {
         std::string number_of_cells;
         std::string start;
         std::string filename;
+        for (int iteration=0; iteration < ITERATION_COUNT; iteration++){
+            if (argc == 2) {
+                filename = argv[1];
+                std::ifstream readfile(filename);
+                if (readfile.is_open()) {
+                    std::string fileline, xx, yy;
+                    while (std::getline(readfile, fileline)) {
+                        std::stringstream ss(fileline);
+                        std::getline(ss, xx, ' ');
+                        std::getline(ss, yy, ' ');
 
-        if (argc == 2) {
-            filename = argv[1];
-            std::ifstream readfile(filename);
-            if (readfile.is_open()) {
-                std::string fileline, xx, yy;
-                while (std::getline(readfile, fileline)) {
-                    std::stringstream ss(fileline);
-                    std::getline(ss, xx, ' ');
-                    std::getline(ss, yy, ' ');
+                        x = std::stoi(xx);
+                        y = std::stoi(yy);
 
-                    x = std::stoi(xx);
-                    y = std::stoi(yy);
-
-                    grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = '1';
-                }
-            }
-            start = "y";
-        } else {
-            std::cout << "Enter the number of cells, or 'r' to read cells from file: ";
-            std::cin >> number_of_cells;
-            std::cout << std::endl;
-
-            if (number_of_cells == "r") {
-                while (true) {
-                    std::cout << "Enter name of file to read from: ";
-                    std::cin >> filename;
-
-                    std::ifstream readfile(filename);
-                    if (readfile.is_open()) {
-                        std::string fileline, xx, yy;
-                        while (std::getline(readfile, fileline)) {
-                            std::stringstream ss(fileline);
-                            std::getline(ss, xx, ' ');
-                            std::getline(ss, yy, ' ');
-
-                            x = std::stoi(xx);
-                            y = std::stoi(yy);
-
-                            grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = '1';
-                        }
-                        break;
-                    } else {
-                        std::cout << "File not found :(" << std::endl;
+                        grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = '1';
                     }
                 }
+                start = "y";
             } else {
-                for (int i = 0; i < std::stoi(number_of_cells); i++) {
-                    std::cout << "Cell " << i + 1 << "/" << number_of_cells << "\nEnter the x & y coordinates: ";
-                    std::cin >> x >> y;
-                    grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = '1';
-                    printGrid(grid, GRID_SIZE, GRID_SIZE);
-                }
-            }
-            std::cout << "Grid setup is done. Start the game? (y/n): ";
-            std::cin >> start;
-        }
+                std::cout << "Enter the number of cells, or 'r' to read cells from file: ";
+                std::cin >> number_of_cells;
+                std::cout << std::endl;
 
-        int incrementor = 0;
-        if (start == "y" || start == "Y") {
-            printGridToFile(grid, GRID_SIZE, GRID_SIZE, "./outputs/output_grid_000000");
-            int iteration = 0;
-            while (iteration < MAX_ITERATIONS) {
-                std::cout <<"---------------------------------------------\n"<<"---------------------------------------------\n"<<"---------------------------------------------\n"<<"ITERATION: "<< iteration << std::endl;
-                iteration +=1;
-                // printGrid(grid, GRID_SIZE, GRID_SIZE);
-                for (int rank = 1; rank < comm_sz; ++rank) {
-                    int startRow = (rank - 1) * rowcount_per_mpi;
-                    int endRow = rank * rowcount_per_mpi;
+                if (number_of_cells == "r") {
+                    while (true) {
+                        std::cout << "Enter name of file to read from: ";
+                        std::cin >> filename;
 
-                    int prevRow = (startRow - 1 + GRID_SIZE) % GRID_SIZE;
-                    int nextRow = (endRow + GRID_SIZE) % GRID_SIZE;
+                        std::ifstream readfile(filename);
+                        if (readfile.is_open()) {
+                            std::string fileline, xx, yy;
+                            while (std::getline(readfile, fileline)) {
+                                std::stringstream ss(fileline);
+                                std::getline(ss, xx, ' ');
+                                std::getline(ss, yy, ' ');
 
-                    std::vector<char> localGrid(elements_received_count);
+                                x = std::stoi(xx);
+                                y = std::stoi(yy);
 
-                    for (int i = 0; i < x_size + 2; ++i) {
-                        // int sourceRow = (prevRow + i) % GRID_SIZE;
-                        int startIdx = getIndex(prevRow+i, 0, GRID_SIZE, GRID_SIZE);
-                        int targetIdx = i * y_size;
-
-                        std::copy(
-                            grid.begin() + startIdx, 
-                            grid.begin() + startIdx + y_size, 
-                            localGrid.begin() + targetIdx
-                        );
+                                grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = '1';
+                            }
+                            break;
+                        } else {
+                            std::cout << "File not found :(" << std::endl;
+                        }
                     }
-                    // std::cout << "Sending from Process Rank: " << my_rank << "\n"
-                    // << " Name: elements expected count Value: " << localGrid.size() << "\n"
-                    // << " Name: end row: " << nextRow << "\n"
-                    // << " Name: start row: " << startRow << "\n"
-                    // << " Name: prevRow: " << prevRow << "\n"
-                    // << " Name: nextRow: " << nextRow << "\n"
-
-                    //<< "---------------------------------------------" << std::endl;
-                    // I think this should work. I hope this works. If this doesn't work I give up
-                    //MPI_Send(localGrid.data(), localGrid.size(), MPI_CHAR, rank, 0, MPI_COMM_WORLD);
-                    MPI_Send(localGrid.data(), elements_received_count, MPI_CHAR, rank, 0, MPI_COMM_WORLD);
-                    // std::cout << "Sent" << std::endl;
-
+                } else {
+                    for (int i = 0; i < std::stoi(number_of_cells); i++) {
+                        std::cout << "Cell " << i + 1 << "/" << number_of_cells << "\nEnter the x & y coordinates: ";
+                        std::cin >> x >> y;
+                        grid[getIndex(x, y, GRID_SIZE, GRID_SIZE)] = '1';
+                        printGrid(grid, GRID_SIZE, GRID_SIZE);
+                    }
                 }
-                MPI_Barrier(MPI_COMM_WORLD);
-
-                for (int rank = 1; rank < comm_sz; ++rank) {
-                    int start = getIndex((rank - 1) * (rowcount_per_mpi), 0, GRID_SIZE, GRID_SIZE);
-                    int end = getIndex(((rank) * rowcount_per_mpi) -1, GRID_SIZE-1, GRID_SIZE, GRID_SIZE);
-                    // std::cout << "Process Rank: " << my_rank << "\n"
-                    // << " Name: elements expected count Value: " << end - start << "\n"
-                    // << " Name: end Value: " << end << "\n"
-                    // << " Name: start Value: " << start << "\n"
-                    // << " Name: elements_rec_count: " << elements_received_count << "\n"
-                    // << " Name: rowcount_per_mpi: " << rowcount_per_mpi << "\n"
-
-                    //<< "---------------------------------------------" << std::endl;
-                    //OLD : //MPI_Recv(grid.data() + start, end - start, MPI_CHAR, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    MPI_Recv(grid.data() + start, (x_size * y_size), MPI_CHAR, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-                }
-                MPI_Barrier(MPI_COMM_WORLD);
-
-                // std::ostringstream filenameStream;
-                // filenameStream << "./outputs/output_grid_" << std::setw(4) << std::setfill('0') << incrementor << ".txt";
-                // std::string outputFilename = filenameStream.str();
-                // printGridToFile(grid, GRID_SIZE, GRID_SIZE, outputFilename);
-                incrementor++;
-
-                // usleep(200000);
-                // clearScreen();
+                std::cout << "Grid setup is done. Start the game? (y/n): ";
+                std::cin >> start;
             }
-            std::ostringstream filenameStream;
-            filenameStream << "./outputs/output_grid_final" << ".txt";
-            std::string outputFilename = filenameStream.str();
-            printGridToFile(grid, GRID_SIZE, GRID_SIZE, outputFilename);
-        } else {
-            return 0;
+
+            int incrementor = 0;
+            if (start == "y" || start == "Y") {
+                printGridToFile(grid, GRID_SIZE, GRID_SIZE, "./outputs/output_grid_000000");
+                int iteration = 0;
+                mainTimer.start();
+                while (iteration < MAX_ITERATIONS) {
+                    // std::cout <<"---------------------------------------------\n"<<"---------------------------------------------\n"<<"---------------------------------------------\n"<<"ITERATION: "<< iteration << std::endl;
+                    iteration +=1;
+                    // printGrid(grid, GRID_SIZE, GRID_SIZE);
+                    for (int rank = 1; rank < comm_sz; ++rank) {
+                        int startRow = (rank - 1) * rowcount_per_mpi;
+                        int endRow = rank * rowcount_per_mpi;
+
+                        int prevRow = (startRow - 1 + GRID_SIZE) % GRID_SIZE;
+                        int nextRow = (endRow + GRID_SIZE) % GRID_SIZE;
+
+                        std::vector<char> localGrid(elements_received_count);
+
+                        for (int i = 0; i < x_size + 2; ++i) {
+                            // int sourceRow = (prevRow + i) % GRID_SIZE;
+                            int startIdx = getIndex(prevRow+i, 0, GRID_SIZE, GRID_SIZE);
+                            int targetIdx = i * y_size;
+
+                            std::copy(
+                                grid.begin() + startIdx, 
+                                grid.begin() + startIdx + y_size, 
+                                localGrid.begin() + targetIdx
+                            );
+                        }
+                        // std::cout << "Sending from Process Rank: " << my_rank << "\n"
+                        // << " Name: elements expected count Value: " << localGrid.size() << "\n"
+                        // << " Name: end row: " << nextRow << "\n"
+                        // << " Name: start row: " << startRow << "\n"
+                        // << " Name: prevRow: " << prevRow << "\n"
+                        // << " Name: nextRow: " << nextRow << "\n"
+
+                        //<< "---------------------------------------------" << std::endl;
+                        // I think this should work. I hope this works. If this doesn't work I give up
+                        //MPI_Send(localGrid.data(), localGrid.size(), MPI_CHAR, rank, 0, MPI_COMM_WORLD);
+                        MPI_Send(localGrid.data(), elements_received_count, MPI_CHAR, rank, 0, MPI_COMM_WORLD);
+                        // std::cout << "Sent" << std::endl;
+
+                    }
+                    MPI_Barrier(MPI_COMM_WORLD);
+
+                    for (int rank = 1; rank < comm_sz; ++rank) {
+                        int start = getIndex((rank - 1) * (rowcount_per_mpi), 0, GRID_SIZE, GRID_SIZE);
+                        int end = getIndex(((rank) * rowcount_per_mpi) -1, GRID_SIZE-1, GRID_SIZE, GRID_SIZE);
+                        // std::cout << "Process Rank: " << my_rank << "\n"
+                        // << " Name: elements expected count Value: " << end - start << "\n"
+                        // << " Name: end Value: " << end << "\n"
+                        // << " Name: start Value: " << start << "\n"
+                        // << " Name: elements_rec_count: " << elements_received_count << "\n"
+                        // << " Name: rowcount_per_mpi: " << rowcount_per_mpi << "\n"
+
+                        //<< "---------------------------------------------" << std::endl;
+                        //OLD : //MPI_Recv(grid.data() + start, end - start, MPI_CHAR, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                        MPI_Recv(grid.data() + start, (x_size * y_size), MPI_CHAR, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                    }
+                    MPI_Barrier(MPI_COMM_WORLD);
+
+                    // std::ostringstream filenameStream;
+                    // filenameStream << "./outputs/output_grid_" << std::setw(4) << std::setfill('0') << incrementor << ".txt";
+                    // std::string outputFilename = filenameStream.str();
+                    // printGridToFile(grid, GRID_SIZE, GRID_SIZE, outputFilename);
+                    incrementor++;
+
+                    // usleep(200000);
+                    // clearScreen();
+                }
+                mainTimer.stop();
+                std::ostringstream filenameStream;
+                filenameStream << "./outputs/output_grid_final" << ".txt";
+                std::string outputFilename = filenameStream.str();
+                printGridToFile(grid, GRID_SIZE, GRID_SIZE, outputFilename);
+            } else {
+                return 0;
+            }
         }
     }else{
-        int iteration = 0;
-        while (iteration < MAX_ITERATIONS){
-            iteration +=1;
-            // usleep(200000 * my_rank);
-            // std::cout << "Process Rank: " << my_rank << "\n"
-            // << " Name: elements received count Value: " << elements_received_count << "\n"
-            // << "---------------------------------------------" << std::endl;
-            std::vector<char> localGrid(elements_received_count);
-            // std::cout << "Process Rank: " << my_rank << "\n" << "Received count:" << elements_received_count << std::endl;
+        for (int it=0; it < ITERATION_COUNT; it++){
 
-            MPI_Recv(localGrid.data(), elements_received_count, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            // std::cout << "received data"<< std::endl;
-            determineState(localGrid, x_size, y_size);
-            // for(int k = 0; k < (x_size+2)*y_size; k++){
-            //     std::cout << localGrid[k];
-            // }
-            // std::cout << "Process Rank: " << my_rank << "\n"
-            // << "Name: elements sent count Value: " << x_size * y_size << "\n" << "iteration" << iteration << "\n";
-            MPI_Barrier(MPI_COMM_WORLD);
-            // << "---------------------------------------------" << std::endl;
-            // To check:
-            MPI_Send(localGrid.data() + y_size, (x_size * y_size), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-            MPI_Barrier(MPI_COMM_WORLD);
+            int iteration = 0;
+            while (iteration < MAX_ITERATIONS){
+                iteration +=1;
+                // usleep(200000 * my_rank);
+                // std::cout << "Process Rank: " << my_rank << "\n"
+                // << " Name: elements received count Value: " << elements_received_count << "\n"
+                // << "---------------------------------------------" << std::endl;
+                std::vector<char> localGrid(elements_received_count);
+                // std::cout << "Process Rank: " << my_rank << "\n" << "Received count:" << elements_received_count << std::endl;
 
+                MPI_Recv(localGrid.data(), elements_received_count, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                // std::cout << "received data"<< std::endl;
+                determineState(localGrid, x_size, y_size);
+                // for(int k = 0; k < (x_size+2)*y_size; k++){
+                //     std::cout << localGrid[k];
+                // }
+                // std::cout << "Process Rank: " << my_rank << "\n"
+                // << "Name: elements sent count Value: " << x_size * y_size << "\n" << "iteration" << iteration << "\n";
+                MPI_Barrier(MPI_COMM_WORLD);
+                // << "---------------------------------------------" << std::endl;
+                // To check:
+                MPI_Send(localGrid.data() + y_size, (x_size * y_size), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+                MPI_Barrier(MPI_COMM_WORLD);
+
+            }
         }
     }
 }
